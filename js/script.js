8d07243da83ee28d79945db9416acb1b14ac41bf
TomasMertian <mertiantomas@gmail.com>
2026-04-17 15:16:04 -0300

    cambie los comentarios para que se entienda mejor la funcion

--- js/script.js
+++ js/script.js
@@ -4,11 +4,11 @@ const menu = document.querySelector(".menu-desplegable");
 // crea una variable llamada boton, y agarra elemento con clase .menu-boton del html
 const boton = document.querySelector(".menu-boton");
 
-// define una funcion para alternar menu, le asigna la clase "cerrado" al body
+// define una funcion para alternar menu, le agrega o quita la clase "cerrado" al body
 function alternarMenu() {
     document.body.classList.toggle("cerrado");
 }
 
-/* selecciona un evento al hacer click en la constante "boton" creada anteriormente,
+/* agrega un evento al hacer click en  el boton seleccionado anteriormente,
 y al registrar el click llama a la funcion alternarMenu */
 boton.addEventListener("click", alternarMenu);
